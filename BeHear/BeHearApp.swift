21097b99486bd6d3f309a5c8cc63d9ce6b3b95ff
Tenzin Choerab <tlobchoerab@gmail.com>
2023-01-11 15:36:01 -0500

    Initial Commit

---
+++ BeHear/BeHearApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeHearApp.swift
+//  BeHear
+//
+//  Created by Tenzin Choerab on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BeHearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
